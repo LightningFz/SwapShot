c63e04297c90e8f227d7cb7713bca1de39c118db
LightningFz <katfawaz64@gmail.com>
2023-01-18 14:32:11 -0500

    new button and a new powerup

--- My project/Assets/Scripts/door&buttons/openDoor.cs
+++ My project/Assets/Scripts/door&buttons/openDoor.cs
@@ -7,7 +7,7 @@ public class openDoor : MonoBehaviour
     public door door;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (collision.CompareTag("Player") || collision.CompareTag("pet box") || collision.CompareTag("box"))
         {
             if (door.doorClosed == true)
             {
