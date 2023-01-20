dd6c16ef4760dd748124c5e25d5511734b8263f1
LightningFz <katfawaz64@gmail.com>
2023-01-19 22:02:52 -0500

    petskins

--- My project/Assets/Scripts/player&pet - Fawaz & Yusuf/petColor.cs
+++ My project/Assets/Scripts/player&pet - Fawaz & Yusuf/petColor.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class petColor : MonoBehaviour
 {
+    public SpriteRenderer petImage;
+    public Color[] color;
+    public Color defaultColor;
+    public bool changePetColor;
+    private int randomizer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        randomizer = Random.Range(0, color.Length);
+        if(changePetColor == true)
+        {
+            petImage.color = color[randomizer];
+        }else if(changePetColor == false)
+        {
+            petImage.color = defaultColor;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (changePetColor == false)
+        {
+            petImage.color = defaultColor;
+        }
     }
 }
