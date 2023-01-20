34c73ed21d986f0f3dda8da563cbdd7c8c2ac506
LightningFz <katfawaz64@gmail.com>
2023-01-20 03:54:52 -0500

    player color for hamza

--- My project/Assets/Scripts/player&pet - Fawaz & Yusuf & Hamza/playerColor.cs
+++ My project/Assets/Scripts/player&pet - Fawaz & Yusuf & Hamza/playerColor.cs
@@ -1,39 +1,38 @@
-// made by Bilal
+//made by Hamza
 
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class backgroundColor : MonoBehaviour
+public class playerColor : MonoBehaviour
 {
-
     //variables
-    public Camera camera;
+    public SpriteRenderer player;
     public Color[] color;//array
     public Color defaultColor;
-    public bool changeBackgroundColor;
+    public bool changePlayerColor;
     private int randomizer;
 
     // Start is called before the first frame update
     void Start()
     {
         randomizer = Random.Range(0, color.Length);//get a random number between 0 and the total amout of colors there are 
-        if (changeBackgroundColor == true)
+        if (changePlayerColor == true)
         {
-            camera.backgroundColor = color[randomizer];//change the background color to a random color from the array
+            player.color = color[randomizer];//change the background color to a random color from the array
         }
-        else if (changeBackgroundColor == false)
+        else if (changePlayerColor == false)
         {
-            camera.backgroundColor = defaultColor;//change the background color to the default one
+            player.color = defaultColor;//change the background color to the default one
         }
     }
 
     // Update is called once per frame
     void Update()
     {   //check if the player wants to change the background color
-        if (changeBackgroundColor == false)
+        if (changePlayerColor == false)
         {
-            camera.backgroundColor = defaultColor;
+            player.color = defaultColor;
         }
     }
 }
