17dd2ec1a05e93e3c4acc6c8c862f50dda362173
Funpolo <mohammedimad.2011@gmail.com>
2023-01-19 20:49:34 -0500

    Comments

--- My project/Assets/Scripts/door&buttons - Fawaz & Faraz/holdPortalButton.cs
+++ My project/Assets/Scripts/door&buttons - Fawaz & Faraz/holdPortalButton.cs
@@ -14,29 +14,29 @@ public class holdPortalButton : MonoBehaviour
     public Color off;
     private void Start()
     {
-        if (isButtonOn == false)
+        if (isButtonOn == false) //checks if the bool "isButtonOn" is false to run the code below 
         {
-            button.color = off;
+            button.color = off; // will turn off the color on the button sprite 
         }
     }
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision) // makes a collider 
     {
-        if(collision.CompareTag("Player") || collision.CompareTag("pet box") || collision.CompareTag("box"))
+        if(collision.CompareTag("Player") || collision.CompareTag("pet box") || collision.CompareTag("box")) //the collider will check if a player or a pet or a box to enter it 
         {
-            AudioSource.PlayClipAtPoint(portalButtonPress,transform.position);
-            isButtonOn = true;
+            AudioSource.PlayClipAtPoint(portalButtonPress,transform.position); // Sound effect will play at that the position of the button 
+            isButtonOn = true; // will turn the bool to true 
             portal.portalOn = true;
-            button.color = on;
+            button.color = on; // will turn on the button color to 
         }
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player") || collision.CompareTag("pet box") || collision.CompareTag("box"))
+        if (collision.CompareTag("Player") || collision.CompareTag("pet box") || collision.CompareTag("box")) //the collider will check if a player or a pet or a box to exit it 
         {
-            AudioSource.PlayClipAtPoint(portalButtonPress,transform.position);
-            isButtonOn = false;
+            AudioSource.PlayClipAtPoint(portalButtonPress,transform.position); // Sound effect will play at that the position of the button 
+            isButtonOn = false; // will turn the bool to true
             portal.portalOn = false;
-            button.color = off;
+            button.color = off; // will turn off the button color to 
         }
     }
 }
