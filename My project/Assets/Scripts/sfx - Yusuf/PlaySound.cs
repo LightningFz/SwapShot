5a86194505f303041322a2fcbb3a7f6f0ee05d0c
LightningFz <katfawaz64@gmail.com>
2023-01-20 03:05:20 -0500

    menu 1-4 fixed

--- My project/Assets/Scripts/sfx - Yusuf/PlaySound.cs
+++ My project/Assets/Scripts/sfx - Yusuf/PlaySound.cs
@@ -2,32 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlaySound : MonoBehaviour
+public class playSound : MonoBehaviour
 {
+    //made by yusuf
+
+    //variables
     public AudioSource petShoot;
     public AudioSource audioSource;
     public AudioSource pauseSound;
     public movement _isJumping;
     public movement isGrounded;
-    // Start is called before the first frame update
-        
-    void Awake()
-    {
-       
-    }
-
-   
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    public void Play()
-    {
-        
-
-    }
     
 } 
--- My project/Assets/Scripts/sfx - Yusuf/VolumeSlider.cs
+++ My project/Assets/Scripts/sfx - Yusuf/VolumeSlider.cs
@@ -4,22 +4,24 @@ using UnityEngine;
 
 public class VolumeSlider : MonoBehaviour
 {
+    // made by yusuf 
+
     public AudioSource AudioSource;
 
     private float musicVolume = 0.29f;
     // Start is called before the first frame update
     void Start()
     {
-        AudioSource.Play();
+        AudioSource.Play();//play music from the start of the game
     }
 
     // Update is called once per frame
     void Update()
     {
-        AudioSource.volume = musicVolume;
+        AudioSource.volume = musicVolume; //updating the volume if the player used the slider 
     }
 
-    public void updateVolume(float volume)
+    public void updateVolume(float volume) //get the new volume inputted by the player 
     {
         musicVolume = volume;
     }
