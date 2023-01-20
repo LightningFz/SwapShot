25e6e34dd14dfb8828cf70d6894d9dede393c3e7
LightningFz <katfawaz64@gmail.com>
2023-01-19 19:59:27 -0500

    fixing the fan

--- My project/Assets/Scripts/levelBuilding - Bilal/fan.cs
+++ My project/Assets/Scripts/levelBuilding - Bilal/fan.cs
@@ -7,22 +7,22 @@ public class fan : MonoBehaviour
     public bool fanOn = false;
     public bool fanDirection = false; 
     public int direction = 1; // the direction the fan will be aim in to push the player 
-    public int fanPower = 5; // How Strong the fan will be pushing the player 
-
+    public int fanPowerAgainstPlayer = 20; // How Strong the fan will be pushing the player 
+    public int fanPowerAgainstBox = 5;// How Strong the fan will be pushing the box 
     // Checks if the player has entered the collider on the fan 
     void OnTriggerStay2D(Collider2D other) {
         if (other.CompareTag("Player")) // Checks if a box has entered the collider on the fan if so will run the code under
         {
             if (fanOn == true) //Will check if the variable "FanOn" is True 
             {
-                other.GetComponent<Rigidbody2D>().AddForce(Vector2.right * fanPower * direction); // Gets the rigibody component for the fan and adds force to it on the x axis using the "Fanpower" variable 
+                other.GetComponent<Rigidbody2D>().AddForce(Vector2.right * fanPowerAgainstPlayer * direction); // Gets the rigibody component for the fan and adds force to it on the x axis using the "fanPowerAgainstPlayer" variable 
             }
         }
         if (other.CompareTag("box")) // Checks if a box has entered the collider on the fan if so will run the code under
         {
             if (fanOn == true)  //Will check if the variable "FanOn" is True 
             {
-                other.GetComponent<Rigidbody2D>().AddForce(Vector2.right * fanPower * direction, ForceMode2D.Impulse); // Gets the rigibody component for the box and adds force to it on the x axis using the "Fanpower" variable 
+                other.GetComponent<Rigidbody2D>().AddForce(Vector2.right * fanPowerAgainstBox * direction, ForceMode2D.Impulse); // Gets the rigibody component for the box and adds force to it on the x axis using the "fanPowerAgainstBox" variable 
             }
         }
     }
